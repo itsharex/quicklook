ab83adfa0c64d61f7da9cf2d16933dbe4a22c932
GuoJiKun <guojikun486546@163.com>
2024-10-21 17:10:38 +0800

    feat: 添加基础文件的预览功能

---
+++ src/views/home/image.vue
@@ -0,0 +1,35 @@
+not-support.vue<script setup lang="ts">
+defineOptions({
+    name: "ImageSupport",
+})
+
+interface Props {
+    src?: string
+}
+const props = withDefaults(defineProps<Props>(),{
+    src: ""
+})
+</script>
+
+<template>
+<div class="image-support">
+    <div>
+        <img :src="props.src" alt="">
+    </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+.image-support {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    & img {
+        max-width: 100%;
+        max-height: 100%;
+    }
+}
+</style>
---
+++ src/views/home/video.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineOptions({
+    name: "VideoSupport",
+})
+
+interface Props {
+    src?: string
+}
+const props = withDefaults(defineProps<Props>(),{
+    src: ""
+})
+</script>
+
+<template>
+    <div class="video-support">
+        <div>
+            <video :src="props.src" controls></video>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.video-support {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    & video {
+        max-width: 100%;
+        max-height: 100%;
+    }
+}
+</style>
