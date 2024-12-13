a05c6dadec3fb58cad6b9671df07ee4e56100cfb
GuoJiKun <guojikun486546@163.com>
2024-12-13 15:14:17 +0800

    wip: 暂存

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -29,3 +29,36 @@ pub fn document(path: &str, mode: &str) -> Result<Vec<helper::DSheet>, String> {
         _ => Err("Not Support".to_string()),
     }
 }
+
+use windows::{
+    core::s,
+    Win32::{
+        Foundation::RECT,
+        UI::WindowsAndMessaging::{self},
+    },
+};
+#[tauri::command]
+pub fn set_into_taskbar(app: AppHandle, label: String) {
+    let w = app.get_webview_window(&label).unwrap();
+    let h = w.hwnd().unwrap();
+    set_taskbar(h);
+}
+
+fn set_taskbar(h: HWND) {
+    unsafe {
+        let shell_tray_wnd = WindowsAndMessaging::FindWindowA(s!("Shell_TrayWnd"), None).unwrap();
+        let tray =
+            WindowsAndMessaging::FindWindowExA(shell_tray_wnd, None, s!("TrayNotifyWnd"), None)
+                .unwrap();
+        let rect = &mut RECT {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+        } as *mut RECT;
+        let _ = WindowsAndMessaging::GetWindowRect(tray, rect);
+        let r = *rect;
+        let _ = WindowsAndMessaging::SetParent(h, shell_tray_wnd);
+        let _ = WindowsAndMessaging::MoveWindow(h, r.left - 100, 0, 100, 60, false);
+    }
+}
