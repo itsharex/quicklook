c5c698e4ad8de56481160384badbdbeeb53890be
GuoJiKun <guojikun486546@163.com>
2024-12-27 13:14:18 +0800

    feat: 优化设置界面

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-import { writeFile, BaseDirectory } from '@tauri-apps/plugin-fs'
+import { BaseDirectory } from '@tauri-apps/plugin-fs'
 // import { emit } from '@tauri-apps/api/event'
 import { readTextFile } from '@/utils'
 
@@ -20,77 +20,31 @@ import { readTextFile } from '@/utils'
 
 const getConfig = async () => {
     const config = await readTextFile('config.json', { baseDir: BaseDirectory.Resource })
-    try {
-        JSON.parse(config)
-    } catch (e) {
-        console.error(e)
-    }
     const data = JSON.parse(config)
-    console.log(typeof config, data)
-    return {
-        markdown: data['preview.markdown'],
-        markdown_checked: data['preview.markdown.checked'],
-        image: data['preview.image'],
-        image_checked: data['preview.image.checked'],
-        video: data['preview.video'],
-        video_checked: data['preview.video.checked'],
-        doc: data['preview.doc'],
-        doc_checked: data['preview.doc.checked'],
-        code: data['preview.code'],
-        code_checked: data['preview.code.checked'],
-        font: data['preview.font'],
-        font_checked: data['preview.font.checked'],
-        archive: data['preview.archive'],
-        archive_checked: data['preview.archive.checked'],
-        book: data['preview.book'],
-        book_checked: data['preview.book.checked'],
-    }
+    const target = [
+        { name: 'markdown', code: 'md', data: data['preview.markdown.cheked'] },
+        { name: '图片', code: 'image', data: data['preview.image.checked'] },
+        { name: '视频', code: 'video', data: data['preview.video.checked'] },
+        { name: '文档', code: 'doc', data: data['preview.doc.checked'] },
+        { name: '代码', code: 'code', data: data['preview.code.checked'] },
+        { name: '字体', code: 'font', data: data['preview.font.checked'] },
+        { name: '压缩包', code: 'archive', data: data['preview.archive.checked'] },
+        { name: '书籍', code: 'book', data: data['preview.book.checked'] },
+    ]
+    return target
 }
 
 interface Config {
-    markdown: Array<string>
-    markdown_checked: Array<string>
-    image: Array<string>
-    image_checked: Array<string>
-    video: Array<string>
-    video_checked: Array<string>
-    doc: Array<string>
-    doc_checked: Array<string>
-    code: Array<string>
-    code_checked: Array<string>
-    font: Array<string>
-    font_checked: Array<string>
-    archive: Array<string>
-    archive_checked: Array<string>
-    book: Array<string>
-    book_checked: Array<string>
+    name: string
+    code: string
+    data: Array<string>
 }
 
-const config = ref<Config>({
-    markdown: [],
-    markdown_checked: [],
-    image: [],
-    image_checked: [],
-    video: [],
-    video_checked: [],
-    doc: [],
-    doc_checked: [],
-    code: [],
-    code_checked: [],
-    font: [],
-    font_checked: [],
-    archive: [],
-    archive_checked: [],
-    book: [],
-    book_checked: [],
-})
+const config = ref<Array<Config>>()
 onMounted(async () => {
     config.value = await getConfig()
     console.log(config.value)
 })
-
-// const config.image_checked = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'apng']
-// const imageChecked = ref([])
 </script>
 
 <template>
@@ -98,61 +52,20 @@ onMounted(async () => {
         <h1>This is an settings page</h1>
         <div class="setting-item support">
             <h2>支持的格式</h2>
-
-            <div class="support-item">
-                <div class="support-item-header">
-                    <span>图像（Image）</span>
-                </div>
-                <div class="support-item-body">
-                    <el-space class="support-item-body-wrap">
-                        <template v-for="item in config.image_checked" :key="item">
-                            <div class="support-item-body-item">
-                                {{ item }}
-                            </div>
-                        </template>
-                    </el-space>
-                </div>
-            </div>
-            <div class="support-item">
-                <div class="support-item-header">
-                    <span>视频（Video）</span>
-                </div>
-                <div class="support-item-body">
-                    <el-space class="support-item-body-wrap">
-                        <template v-for="item in config.video_checked" :key="item">
-                            <div class="support-item-body-item">
-                                {{ item }}
-                            </div>
-                        </template>
-                    </el-space>
-                </div>
-            </div>
-            <div class="support-item">
-                <div class="support-item-header">
-                    <span>字体（Font）</span>
-                </div>
-                <div class="support-item-body">
-                    <el-space class="support-item-body-wrap">
-                        <template v-for="item in config.font_checked" :key="item">
-                            <div class="support-item-body-item">
-                                {{ item }}
-                            </div>
-                        </template>
-                    </el-space>
-                </div>
-            </div>
-            <div class="support-item">
-                <div class="support-item-header">
-                    <span>书籍（Book）</span>
-                </div>
-                <div class="support-item-body">
-                    <el-space class="support-item-body-wrap">
-                        <template v-for="item in config.book_checked" :key="item">
-                            <div class="support-item-body-item">
-                                {{ item }}
-                            </div>
-                        </template>
-                    </el-space>
+            <div>
+                <div class="support-item" v-for="type in config" :key="type.code">
+                    <div class="support-item-header">
+                        <span>{{ type.name }}（{{ type.code }}）</span>
+                    </div>
+                    <div class="support-item-body">
+                        <el-space class="support-item-body-wrap">
+                            <template v-for="item in type.data" :key="item">
+                                <div class="support-item-body-item">
+                                    {{ item }}
+                                </div>
+                            </template>
+                        </el-space>
+                    </div>
                 </div>
             </div>
         </div>
