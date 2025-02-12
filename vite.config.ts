1ebd35bc61f68b2a3e2cd83cd0eca46227be1f56
GuoJiKun <guojikun486546@163.com>
2025-02-12 11:38:59 +0800

    feat: 增加 sentry 监控配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,49 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+import pkg from './package.json'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueJsx()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [
+            vue(),
+            vueJsx(),
+            sentryVitePlugin({
+                org: 'zhiqiu',
+                project: 'quicklook-vue',
+                authToken: env.VITE_SENTRY_TOKEN,
+                sourcemaps: {
+                    filesToDeleteAfterUpload: ['dist/**/*.map'],
+                },
+                release: {
+                    name: pkg.version || 'default',
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vender: ['vue', 'vue-router', 'pinia'],
+        build: {
+            sourcemap: true,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vender: ['vue', 'vue-router', 'pinia'],
+                    },
                 },
             },
         },
-    },
-    server: {
-        host: true,
-        port: 6688,
-    },
+        server: {
+            host: true,
+            port: 6688,
+        },
+    }
 })
