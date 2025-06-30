d08d51164132b18be168a8938dcdcaab87dd947d
_zhiqiu <guojikun486546@163.com>
2025-06-30 11:00:49 +0800

    fix: 修复预览图片时不能自动居中的问题 (#25)

--- src/views/preview/image.vue
+++ src/views/preview/image.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { Leafer, Image } from 'leafer-ui'
+import { App, Image, ImageEvent } from 'leafer-ui'
+import '@leafer-in/view'
+import '@leafer-in/viewport'
+
 import { useRoute } from 'vue-router'
 import type { FileInfo } from '@/utils/typescript'
 import LayoutPreview from '@/components/layout-preview.vue'
 import { convertFileSrc } from '@tauri-apps/api/core'
+import { ElMessage } from 'element-plus'
 
 defineOptions({
     name: 'ImageSupport',
@@ -12,27 +16,44 @@ defineOptions({
 
 const route = useRoute()
 
-let leafer: Leafer | null = null
+let leafer: App | null = null
 let image: Image | null = null
 const fileInfo = ref<FileInfo>()
 
+const loading = ref(false)
 const init = (path: string) => {
+    loading.value = true
     if (leafer !== null) {
         leafer?.destroy()
         image?.destroy()
     }
-    leafer = new Leafer({
+    leafer = new App({
         view: 'canvas', // 支持 window 、div、canvas 标签对象， 可使用id字符串(不用加 # 号)
-        width: 0, // 不能设置为 0， 否则会变成自动布局
-        height: 0,
+        tree: {
+            type: 'viewport', // 视口类型，支持 viewport、window、div、canvas
+        },
     })
 
     image = new Image({
         url: path,
         draggable: true,
+        scale: 1,
+        x: 0,
+        y: 0,
     })
 
-    leafer.add(image)
+    image.load()
+
+    image.on(ImageEvent.LOADED, (ev: ImageEvent) => {
+        console.log('Image loaded:', ev)
+        leafer?.tree.add(image as Image)
+        leafer?.tree.zoom('fit', 0, true)
+        loading.value = false
+    })
+    image.on(ImageEvent.ERROR, () => {
+        ElMessage.error('图片加载失败')
+        loading.value = false
+    })
 }
 onMounted(() => {
     fileInfo.value = route.query as unknown as FileInfo
@@ -41,7 +62,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <LayoutPreview :file="fileInfo">
+    <LayoutPreview :file="fileInfo" :loading="loading">
         <div class="image-support">
             <div class="image-support-inner" id="canvas"></div>
         </div>
@@ -56,9 +77,11 @@ onMounted(() => {
     justify-content: center;
     align-items: center;
     align-content: center;
+
     &-inner {
         width: 100%;
         height: 100%;
+        background-color: #efefef;
     }
 }
 </style>
